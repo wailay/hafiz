6514f7b0536c5005cbe5a2a41e7cd0b8fb3f377d
Wail Ayad <wailay@hotmail.fr>
2025-07-19 13:06:49 -0400

    enabled speedinsights

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import { APP_DESCRIPTION, APP_TITLE } from "./constant";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -25,6 +26,7 @@ export default function RootLayout({
       <body>
         <ThemeProvider>{children}</ThemeProvider>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
